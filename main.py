b99716a566072ca108784e75af39adc773b4a521
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2023-11-16 16:42:33 +0300

    Up convert, file text based on encoding, not label

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.filedialog import asksaveasfile
+from datetime import datetime
+from unidecode import unidecode
  
 root = Tk()
 root.title("ascii-unicode-converter")    
@@ -37,30 +40,49 @@ output.pack()
 convertButton = Label(text='Конвертировать', font=('Arial', 16), background='#5d4fe0', foreground='#ffffff',padx=10, pady=10)
 convertButton.pack()
 
+def saveToFile():
+    print('saving file...')
+    now = datetime.now()
+    print(now)
+    outputText = output.get('1.0', 'end').strip()
+    print(outputText)
+    outputEncoding = convertEncodings(self=saveToFile)
+    print(outputEncoding)
+    if outputEncoding == 'ascii':
+        file = open(f'./files/{now}', 'w', encoding='utf-8')
+    else:
+        file = open(f'./files/{now}', 'w', encoding='ascii')
+    file.write(outputText)
+
+def textFromFile():
+    print('текст из файла')
+
 mainMenu = Menu()
-mainMenu.add_cascade(label='Сохранить в файл')
-mainMenu.add_cascade(label='Текст из файла')
+mainMenu.add_command(label='Сохранить в файл', command= saveToFile)
+mainMenu.add_command(label='Текст из файла', command=textFromFile)
 
 def convertEncodings(self):
     if titleLabel1['text'] == 'ascii':
-        #clear text
-        asciiString = textField.get('1.0', 'end')
+        outputEncoding = 'unicode'
+        asciiString = textField.get('1.0', 'end').strip()
         print(asciiString)
-        unicodeString = str(asciiString)
+        unicodeString = asciiString.encode('utf-8')
         print(unicodeString)
         output.config(state=NORMAL)
         output.delete('1.0', END)
         output.insert('1.0', unicodeString)
         output.config(state=DISABLED)
     else:
-        unicodeString = textField.get('1.0', 'end')
+        unicodeString = textField.get('1.0', 'end').strip()
+        outputEncoding = 'ascii'
         print(unicodeString)
-        asciiString = unicodeString.encode('ascii', 'ignore')
+        asciiString = unidecode(unicodeString)
         print(asciiString)
         output.config(state=NORMAL)
         output.delete('1.0', END)
         output.insert('1.0', asciiString)
         output.config(state=DISABLED)
+    return outputEncoding
 
 convertButton.bind('<Button-1>', convertEncodings)
 
